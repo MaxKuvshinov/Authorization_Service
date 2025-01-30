e60d26b089362ae4c480296ad866bf4a4aea0adb
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2025-01-30 15:58:54 +0300

    added user model and implemented serializers, plus minor improvements

--- users/models.py
+++ users/models.py
@@ -1,14 +1,16 @@
+from typing import List
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-from users.services import generate_invite_code
+import random
+import string
 
 
 class CustomUser(AbstractUser):
     username = None
     phone_number = models.CharField(max_length=15, unique=True, verbose_name="Номер телефона", help_text="Укажите номер телефона")
-    invite_code = models.CharField(max_length=6, unique=True, default=generate_invite_code, verbose_name="Инвайт-код")
-    referral_code = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Пригласительный инвайт код")
+    invite_code = models.CharField(max_length=6, unique=True, verbose_name="Инвайт-код")
+    referral_code = models.CharField(max_length=6, null=True, blank=True, verbose_name="Пригласительный инвайт-код")
 
     USERNAME_FIELD = "phone_number"
     REQUIRED_FIELDS = []
@@ -19,3 +21,21 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.phone_number
+
+    def save(self, *args, **kwargs):
+        if not self.invite_code:
+            while True:
+                invite_code = self.generate_invite_code()
+                if not CustomUser.objects.filter(invite_code=invite_code).exists():
+                    self.invite_code = invite_code
+                    break
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def generate_invite_code():
+        """Функция генерации инвайт кода"""
+        return "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+    def get_referred_users(self) -> List[str]:
+        """Функция возвращающая список номеров телефонов, которые использовали инвайт-код данного пользователя."""
+        return list(CustomUser.objects.filter(referral_code=self.invite_code).values_list('phone_number', flat=True))
--- users/services.py
+++ users/services.py
@@ -1,12 +1,5 @@
-import string
-import random
 from smsaero import SmsAero
 from config.settings import SMSAERO_API_KEY, SMSAERO_EMAIL
-
-
-def generate_invite_code():
-    """Функция генерации инвайт кода"""
-    return "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
 
 
 def send_sms(phone: int, message: str) -> dict:
